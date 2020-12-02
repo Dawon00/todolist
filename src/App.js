c07ab5dd25f8bd8873bea19270bfa57139bff301
Dawon00 <da6921@naver.com>
2020-12-03 00:54:34 +0900

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,58 @@
-import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+  //[변수, 변수를 바꾸는 함수]
+  //새로운 state 변수를 선언하고, val이라고 부르겠습니다
+  //useState 안에 들어가는 건 변수의 초기값
+  const [val, setValue] = useState('');
+  //새로운 state 변수를 선언하고, state 라고 부르겠습니다
+  const [state, setState] = useState([]);
+
+  function handleOnKeyPress(e) {
+    if (e.key === 'Enter') {
+      handleOnSubmit();
+    }
+  }
+
+  function handleOnSubmit() {
+    //setState
+    setState([val,...state]);
+    //setValue
+    setValue('');
+  }
+
+  function handleOnChange(e) {
+    //setValue
+    setValue(e.target.value);
+  }
+
+  function handleRemove(idx) {
+    //setState(spread, slice)
+    setState(
+      [...state.slice(0,idx),...state.slice(idx+1)]
   );
+}
+
+return (
+  <div className='App'>
+    <input
+      value={val}
+      onChange={handleOnChange}
+      onKeyPress={handleOnKeyPress}
+    />
+    <button type='submit' onClick={handleOnSubmit}>
+      등록하기
+    </button>
+    {state.map((e, idx) => (
+      <div key={idx}>
+        <span>{e}</span>
+        &nbsp;
+        <button onClick={() => handleRemove(idx)}>제거</button> 
+      </div>
+    ))}
+  </div>
+);
 }
 
 export default App;
